9d145d45f709b63697580a0a97fe6e316ccf8e3f
5Mountains <V.Piatyhor@gmail.com>
2021-01-12 17:28:46 +0200

    Updates

---
+++ precache-manifest.7f8614463b3f70535c84e1fa95ac616d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea360bdfddb552288ac36dd7cf18bd9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441511eefd5dc0c3c103",
+    "url": "./static/js/main.5eb894de.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.7f8614463b3f70535c84e1fa95ac616d.js"
 );
 
 self.addEventListener('message', (event) => {
